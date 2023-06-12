7eb173bad0e579705a37df3bc6b028552fa57323
FiniteLooper <chris.m.barr@gmail.com>
2023-06-12 14:44:30 -0400

    Add some general purpose utilities we will need

---
+++ src/utils.spec.ts
@@ -0,0 +1,83 @@
+import * as Utils from './utils';
+
+describe('Utils', () => {
+  describe('stripRtf()', () => {
+    it('should get single line plain text from an RTF string', () => {
+      expect(
+        Utils.stripRtf(
+          '{\\rtf1\\ansi\\ansicpg1252\\cocoartf1343\\cocoasubrtf140\n\\cocoascreenfonts1{\\fonttbl\\f0\\fswiss\\fcharset0 Helvetica;}\n{\\colortbl;\\red255\\green255\\blue255;}\n\\pard\\tx560\\tx1120\\tx1680\\tx2240\\tx2800\\tx3360\\tx3920\\tx4480\\tx5040\\tx5600\\tx6160\\tx6720\\pardirnatural\\qc\n\n\\f0\\fs260 \\cf1 \\expnd0\\expndtw0\\kerning0\n\\outl0\\strokewidth0 \\strokec0 Our Good }'
+        )
+      ).toEqual(`Our Good`);
+    });
+
+    it('should get multiline plain text from an RTF string 1', () => {
+      expect(
+        Utils.stripRtf(
+          '{\\rtf1\\ansi\\ansicpg1252\\cocoartf1038\\cocoasubrtf320\n{\\fonttbl\\f0\\fswiss\\fcharset0 Impact;}\n{\\colortbl;\\red255\\green255\\blue255;}\n\\pard\\tx560\\tx1120\\tx1680\\tx2240\\tx2800\\tx3360\\tx3920\\tx4480\\tx5040\\tx5600\\tx6160\\tx6720\\qc\\pardirnatural\n\n\\f0\\fs120 \\cf1 \\outl0\\strokewidth-40 \\strokec0 We bow our hearts we bend our knees\\\nOh Spirit come make us humble\\\nWe turn our eyes from evil things\\\nOh Lord we cast down our idols}'
+        )
+      ).toEqual(`We bow our hearts we bend our knees
+Oh Spirit come make us humble
+We turn our eyes from evil things
+Oh Lord we cast down our idols`);
+    });
+
+    it('should get multiline plain text from an RTF string 2', () => {
+      expect(
+        Utils.stripRtf(
+          '{\\rtf1\\ansi\\ansicpg1252\\cocoartf1343\\cocoasubrtf140\n\\cocoascreenfonts1{\\fonttbl\\f0\\fswiss\\fcharset0 Helvetica;}\n{\\colortbl;\\red255\\green255\\blue255;}\n\\pard\\tx560\\tx1120\\tx1680\\tx2240\\tx2800\\tx3360\\tx3920\\tx4480\\tx5040\\tx5600\\tx6160\\tx6720\\pardirnatural\\qc\n\n\\f0\\fs260 \\cf1 \\outl0\\strokewidth0 \\strokec1 Give us clean hands\\\nGive us pure hearts\\\nLet us not lift our\\\nSouls to another}'
+        )
+      ).toEqual(`Give us clean hands
+Give us pure hearts
+Let us not lift our
+Souls to another`);
+    });
+  });
+
+  describe('mergeArraysByProp()', () => {
+    it('should only combine arrays of objects together when all values of the same key are unique', () => {
+      const obj1 = [{ foo: 'fooVal' }];
+      const obj2 = [{ foo: 'barVal' }];
+      expect(Utils.mergeArraysByProp(obj1, obj2, 'foo')).toEqual([
+        { foo: 'fooVal' },
+        { foo: 'barVal' },
+      ]);
+    });
+
+    it('should merge identical arrays of objects together, overwriting values for the same key', () => {
+      const obj1 = [{ foo: 'fooVal' }];
+      const obj2 = [{ foo: 'fooVal' }];
+      expect(Utils.mergeArraysByProp(obj1, obj2, 'foo')).toEqual([{ foo: 'fooVal' }]);
+    });
+
+    it('should merge different arrays of objects together, overwriting values for the same key', () => {
+      const obj1 = [
+        { foo: 'fooVal1', other: 'originalValue' },
+        { foo: 'fooVal2', other: 'newValue1' },
+        { foo: 'fooVal3', other: 'newValue2' },
+      ];
+      const obj2 = [{ foo: 'fooVal2', other: 'MyOverwrittenValue' }];
+
+      expect(Utils.mergeArraysByProp(obj1, obj2, 'foo')).toEqual([
+        { foo: 'fooVal1', other: 'originalValue' },
+        { foo: 'fooVal3', other: 'newValue2' },
+        { foo: 'fooVal2', other: 'MyOverwrittenValue' },
+      ]);
+    });
+  });
+
+  describe('normalizeLineEndings()', () => {
+    it('should replace all line ending combinations with \\n', () => {
+      //Single instances
+      expect(Utils.normalizeLineEndings('\n\r')).toEqual('\n');
+      expect(Utils.normalizeLineEndings('\r\n')).toEqual('\n');
+
+      //Multiple instances
+      expect(Utils.normalizeLineEndings('\n\r\n\r')).toEqual('\n\n');
+      expect(Utils.normalizeLineEndings('\r\n\r\n')).toEqual('\n\n');
+
+      //No changes expected here
+      expect(Utils.normalizeLineEndings('\n')).toEqual('\n');
+      expect(Utils.normalizeLineEndings('\n\n')).toEqual('\n\n');
+    });
+  });
+});
